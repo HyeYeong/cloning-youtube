59b9e9e40f5d4f8c19c9f1c2c3d2bef06393509f
HyeYeong <todzlfk1@gmail.com>
2023-10-06 10:14:26 +0900

    feat: request and response

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 import express from "express";
-const app = express();
 const PORT = 4000;
+const app = express();
+
+const handleHome = (req, res) => {
+  res.send("hello I am response");
+  return res.end();
+};
+
+const handleLogin = (req, res) => {
+  return res.send("login here");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`server listening on port http://localhost:${PORT} -!`);
