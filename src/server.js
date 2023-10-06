fec1c2874dd6f4f584d09d156b840f83928d367f
HyeYeong <todzlfk1@gmail.com>
2023-10-06 10:37:20 +0900

    feat: middleware

--- src/server.js
+++ src/server.js
@@ -3,15 +3,21 @@ const PORT = 4000;
 const app = express();
 
 const handleHome = (req, res) => {
-  res.send("hello I am response");
+  res.send("I am in the final ware!");
   return res.end();
+};
+
+const gossipMiddleWare = (req, res, next) => {
+  console.log(`someone is trying to go ${req.url}`);
+  // 함수가 next 를 호출한다면 이 함수는 미들웨어라는걸 의미함
+  next();
 };
 
 const handleLogin = (req, res) => {
   return res.send("login here");
 };
 
-app.get("/", handleHome);
+app.get("/", gossipMiddleWare, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () =>
