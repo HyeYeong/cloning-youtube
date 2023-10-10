cf98c9aec90d13d1d83ec2c78f255a4f4eea6d1b
HyeYeong <todzlfk1@gmail.com>
2023-10-10 17:19:14 +0900

    feat: regular expression

--- src/router/videoRouter.js
+++ src/router/videoRouter.js
@@ -9,8 +9,9 @@ videoRouter.get(PATH.UPLOAD, Upload);
 // express 는 id가 변수가 되된다는 것을, 파라미터가 있다는 것을 이해함다.
 // 이 때, 콜론이 들어간 변수는 맨 밑에 와야 한다. 일반적인 패턴의 url이 콜론 변수 사이에 오게 되면 그 일반적인 url도 변수로 인식하게 되어버린다.
 // express는 위에서 아래로 읽기 때문에 일어나는 현상이다.
-videoRouter.get("/:id", See);
-videoRouter.get(`/:id${PATH.EDIT}`, Edit);
-videoRouter.get(`/:id${PATH.DELETE}`, Delete);
+// id 라는 글자는 없어도 되지만, 파라미터로 이용하기 위해서 그냥 남겨둠
+videoRouter.get("/:id(\\d+)", See);
+videoRouter.get(`/:id(\\d+)${PATH.EDIT}`, Edit);
+videoRouter.get(`/:id(\\d+)${PATH.DELETE}`, Delete);
 
 export default videoRouter;
