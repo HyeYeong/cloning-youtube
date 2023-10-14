d0fc76bca05e4710a13c28c1f0fa95318cfc749d
HyeYeong <todzlfk1@gmail.com>
2023-10-14 12:29:16 +0900

    fix: wrong pathName

--- src/router/globalRouter.js
+++ src/router/globalRouter.js
@@ -5,8 +5,8 @@ import { Trending, Search } from "../controllers/videoController";
 const globalRouter = express.Router();
 
 globalRouter.get("/", Trending);
-globalRouter.get("join", Join);
-globalRouter.get("login", Login);
-globalRouter.get("search", Search);
+globalRouter.get("/join", Join);
+globalRouter.get("/login", Login);
+globalRouter.get("/search", Search);
 
 export default globalRouter;
