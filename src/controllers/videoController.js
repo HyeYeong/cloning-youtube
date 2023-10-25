b3d2eb4a9a56e06720b24c989a4a017ad9cf09de
HyeYeong <todzlfk1@gmail.com>
2023-10-25 17:07:50 +0900

    feat: save data -> database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,6 +8,7 @@ export const Home = async (req, res) => {
   // NOTE: promise에서는 에러의 표시를 위해서, try catch문을 사용한다.
   try {
     const videos = await Video.find({});
+    console.log(videos);
 
     return res.render("home", {
       pageTitle: "home",
@@ -25,18 +26,16 @@ export const Search = (req, res) => {
 
 export const Watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
   return res.render("watch", {
-    pageTitle: `watching ${video.title}`,
+    // pageTitle: `watching ${video.title}`,
     video,
   });
 };
 
 export const GetEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
   return res.render("edit", {
-    pageTitle: `Editing: ${video.title}`,
+    // pageTitle: `Editing: ${video.title}`,
     video,
   });
 };
@@ -45,7 +44,7 @@ export const GetEdit = (req, res) => {
 export const PostEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
+  // videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -57,18 +56,25 @@ export const GetUpload = (req, res) => {
   return res.render("upload", { pageTitle: "upload video" });
 };
 
-export const PostUpload = (req, res) => {
+export const PostUpload = async (req, res) => {
   // TODO: 이 곳에서 Videos array를 추가할 예정
-  const newId = videos.length + 1;
-  const { title } = req.body;
-  const newVideoObj = {
+  // const newId = videos.length + 1;
+  const { title, description, hashtags } = req.body;
+
+  const video = new Video({
     title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: newId,
-  };
-  videos.push(newVideoObj);
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  // NOTE: 이 상태에서, 프론트에는 데이터가 저장이 되지만, 아직 데이터베이스에 데이터를 저장해주지는 않았음
+  console.log(video);
+
+  // NOTE: save함수에 마우스 커서를 올려보면, save는 promise를 이용한다, 즉 세이브하는데에 시간이 걸린다는 뜻이므로, await를 적어서 기다려주면 좋다.
+  await video.save();
   return res.redirect(`/`);
 };
