5eb9371f806a4cf97236b6f265490404b2aab9e6
HyeYeong <todzlfk1@gmail.com>
2023-10-25 17:51:44 +0900

    feat: set min, max length and  modify design

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,15 +1,27 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 20,
+    maxLength: 80,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 20,
+    maxLength: 140,
+  },
   createdAt: {
     type: Date,
     required: true,
     default: Date.now,
     //NOTE: Date.now()에 ()를 적지 않는 이유는, 바로 실행시키지 않기 위해서임
   },
-  hashtags: [{ type: String }],
+  hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
